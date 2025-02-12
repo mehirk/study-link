afceaa2f3611b2b213ee707aef268fa6cd58c594
Mehir Kumar <mehirk28@gmail.com>
2025-02-11 23:35:16 -0400

    made front end and backend talk to each other

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
 import "dotenv/config"
+import cors from "cors"; //wanted to accept requests from frontend
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: "http://localhost:5174"
+}));
+
 app.get("/", (req: Request, res: Response) => {
+    console.log("Hi from frontend");
     res.send("Hello, World! 🚀");
 });
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [response, setResponse] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/')
+        const body = await response.text()
+        console.log(body)
+        setResponse(body)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <>
@@ -16,7 +32,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{response}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
