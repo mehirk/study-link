6f1e36cf54512092aa92ed24253c57c9b665575c
Shahbaz Singh <shahbazathwal2107@gmail.com>
2025-02-13 20:04:30 -0400

    light dark works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 // import { authClient } from "./lib/auth-client";
 import { SignupForm } from "@components/sign-up";
+import { ThemeProvider } from "@components/theme-provider";
+import { ModeToggle } from "@components/mode-toggle";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 gap-4">
-      <SignupForm />
-    </div>
+    <ThemeProvider>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100 gap-4">
+        <ModeToggle />
+        <SignupForm />
+      </div>
+    </ThemeProvider>
   );
 };
 
