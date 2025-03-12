b28612c28b09df7c8c93bd78ee2b59cb19cbe9a9
Mehir Kumar <mehirk28@gmail.com>
2025-03-12 20:46:55 -0300

    add footer

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "./theme-provider";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
@@ -10,6 +11,7 @@ const Layout = () => {
         <main className="flex-1">
           <Outlet />
         </main>
+        <Footer />
       </div>
     </ThemeProvider>
   );
